c14d7594b25d9d90c1828c8999c71de569b41a93
webferhat <ferhatyigit@gmail.com>
2025-04-06 22:26:04 +0300

    12

--- 07_ForeachLoop/Program.cs
+++ 07_ForeachLoop/Program.cs
@@ -10,7 +10,18 @@ namespace _07_ForeachLoop
     {
         static void Main(string[] args)
         {
-            ////
+            #region
+            int[] numbers = { 45, 22, 365, 88, 99 };
+
+            foreach (int i in numbers)
+            {
+                if(i % 2 == 0) { 
+                    Console.WriteLine(i);
+                }
+            }
+            #endregion
+
+            Console.Read();
         }
     }
 }
