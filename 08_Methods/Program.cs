b0401cf0b95d2b3a528a0deabddcdaa41c0693bd
webferhat <ferhatyigit@gmail.com>
2025-04-07 23:05:44 +0300

    ego9

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Metotlar
+            //Void metotları
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Ferhat");
+            //    Console.WriteLine("Ahmet");
+            //    Console.WriteLine("Yusuf");
+            //}
+            //CustomerList();
+            #endregion
+
+            #region geriye değer döndürmeyen
+            //void CustomerCard(string name, string surName)
+            //{
+            //    Console.WriteLine("Müşteri: " + name + " " + surName);
+
+            //}
+            //CustomerCard("ferhat", "yiğit");
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
