98e5d17a07782db0cd76825d0de1b6b85f975521
webferhat <ferhatyigit@gmail.com>
2025-04-07 23:28:24 +0300

    789

--- 09_DatabaseProject/Program.cs
+++ 09_DatabaseProject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,29 @@ namespace _09_DatabaseProject
     {
         static void Main(string[] args)
         {
+            string tableNumber;
 
+            Console.Write("tablo numarası girin: ");
+            tableNumber= Console.ReadLine();
+
+            SqlConnection connection = new SqlConnection("Data Source=ASUS-PROART;initial Catalog=EgitimKampiDb;integrated security=true");
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM TblCategory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            connection.Close();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach(var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
         }
+        
     }
 }
